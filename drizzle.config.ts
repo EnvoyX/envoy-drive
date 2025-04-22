e3735b04fabd7f953a5a93e3805786a2f6109cff
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-04-23 00:24:13 +0700

    feat : added Database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,14 @@ import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
+  dialect: "singlestore",
   tablesFilter: ["envoy-drive_*"],
+  dbCredentials: {
+    host: "svc-3482219c-a389-4079-b18b-d50662524e8a-shared-dml.aws-virginia-6.svc.singlestore.com",
+    user: "muhamad hanif-97403",
+    password: "O[]@A-nmLm[9Yq6jI2:o",
+    port: 3333,
+    database: "db_muhamadhanif_44fa1",
+    ssl: {},
+  },
 } satisfies Config;
